f7949f30a63cb2c4570c2c10524f41179cfc9e9f
Son <hashemaoui@gmail.com>
2021-07-07 12:56:56 +0200

    ajout pattern dans le formulaire

--- js/commande.js
+++ js/commande.js
@@ -12,20 +12,15 @@ const remerciementPageCommande = () => {
         document.querySelector('.recapitulatif-commande')
             .innerHTML = `<div class="resume">
                             <h2>Bonjour ${contactOrderIdProductsObject.contact.firstName}. <br> Merci d'avoir passé commande chez orinoco</h2>
+                            <p>Votre commande pour un montant de ${totalPrice}€ a été validé</p><br>
                             <p id="id">Votre identifiant de commande est le : <br><br>
                                 ${contactOrderIdProductsObject.orderId}</p>
-                            <p>Le prix total de vos articles est de ${totalPrice} €</p>
+                            <p>Elle vous sera livré sous 72h à l'adresse suivante : <br><br>
+                                ${contactOrderIdProductsObject.contact.address} à ${contactOrderIdProductsObject.contact.city}</p>
+                               <br> <p style="text-align: right">L'équipe d'Orinoco</p>
+                            
                         </div>`;
 
-        // let return_links = document.querySelectorAll('.return');
-        
-        // for(return_link of return_links){
-        //     return_link.addEventListener('click', () => {
-        //         localStorage.removeItem('products');
-        //         localStorage.removeItem('ContactOrderIdProducts');
-        //     })
-        // }
-        
         localStorage.clear();
 
     } else {
